0d8337b3cba9f3b253a3ea8557d0cde7cc75bba2
Travis Beckham <howdytravis@gmail.com>
2023-04-19 10:35:46 -0500

    cleaned up mobile nav

--- static/js/main.js
+++ static/js/main.js
@@ -1,47 +1,41 @@
 (function(){
-  // Create mobile nav menu (see .js-nav and .nav-toggle in main.css)
+  // Create mobile nav menu (see .js-nav, .js-nav-toggle, and .js-nav-close in main.css)
   var mainNav = document.getElementById('main-nav');
   if (mainNav) {
-    var isNavShowing = false;
     // Add classname to body
-    document.body.className += ' js-nav';// hides .main-nav
-    var bodyClassName = document.body.className;
-    // Create anchor link to toggle the display of .main-nav
-    var toggle = document.createElement('a');
-    toggle.appendChild(document.createTextNode('Menu'));
-    toggle.href = '#';
-    toggle.addEventListener('click', function(e){
-      e.preventDefault();
-      document.body.className = isNavShowing ? bodyClassName : bodyClassName + ' show-nav';
-      isNavShowing = ! isNavShowing;
-    });
-    // Create .nav-toggle
+    document.body.classList.add('js-nav');// Hides .main-nav by default
+    // Create .js-nav-toggle to toggle the display of nav
     var navToggle = document.createElement('div');
-    navToggle.className = 'nav-toggle';
-    navToggle.appendChild(toggle);
-    // Insert .nav-toggle before .main-nav
-    mainNav.parentNode.insertBefore(navToggle, mainNav);
-    // Create close link
-    var close = document.createElement('a');
-    close.appendChild(document.createTextNode('Close'));
-    close.href = '#';
-    close.className = 'close';
-    close.addEventListener('click', function(e){
+    navToggle.className = 'js-nav-toggle';
+    var navToggleLink = document.createElement('span');
+    navToggleLink.appendChild(document.createTextNode('Menu'));
+    navToggleLink.addEventListener('click', function(e){
       e.preventDefault();
-      document.body.className = bodyClassName;
-      isNavShowing = false;
+      document.body.classList.toggle('show-nav');
     });
-    // Insert close as a child of .main-nav
-    mainNav.prepend(close);
+    navToggle.appendChild(navToggleLink);
+    // Insert .js-nav-toggle before .main-nav
+    mainNav.parentNode.insertBefore(navToggle, mainNav);
+    // Create .js-nav-close to close nav
+    var navClose = document.createElement('div');
+    navClose.className = 'js-nav-close';
+    var navCloseLink = document.createElement('span');
+    navCloseLink.appendChild(document.createTextNode('Close'));
+    navCloseLink.addEventListener('click', function(e){
+      e.preventDefault();
+      document.body.classList.remove('show-nav');
+    });
+    navClose.appendChild(navCloseLink);
+    // Insert .js-nav-close as the first child of .main-nav
+    mainNav.prepend(navClose);
   }
   /*
   Result:
   <body class="js-nav">
     ...
-    <div class="nav-toggle"><a href="#">Menu</a></div>
+    <div class="js-nav-toggle"><span>Menu</span></div>
     <nav id="main-nav" class="main-nav">
-      <a href="#" class="close">Close</a>
+      <div class="js-nav-close"><span>Close</span></div>
       <ul>...
-
   */
 }());
